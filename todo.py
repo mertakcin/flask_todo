06fcb6980b68436dc1e890d784da13fa67abf7fb
mertakcin <akcinmert@hotmail.com>
2020-04-27 13:35:08 +0300

    First Update

---
+++ todo.py
@@ -0,0 +1,87 @@
+from flask import Flask,render_template,url_for,redirect,request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:////Users/user/Desktop/ToDoApp/todo.db'
+db = SQLAlchemy(app)
+
+@app.route("/")
+
+def index():
+
+
+    todos = Todo.query.all()
+
+    return render_template("index.html", todos = todos)
+
+
+@app.route("/complete/<string:id>")
+
+def complete(id):
+
+    todo = Todo.query.filter_by(id = id).first()
+    # gathering the data from entry that id = 1
+    
+    todo.complete = not todo.complete
+    
+    '''
+    if todo.complete == True:
+        todo.complete == False
+    else: 
+        todo.complete == True
+        '''
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+
+@app.route("/add", methods = ["POST"])
+
+
+
+def addTodo():
+
+    title = request.form.get("title")
+    newTodo = Todo(title = title, complete = False)
+    db.session.add(newTodo)
+
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+@app.route("/delete/<string:id>")
+
+def deleteTodo(id):
+
+    todo = Todo.query.filter_by(id = id).first()
+    db.session.delete(todo)
+
+    db.session.commit()
+
+    return redirect(url_for("index"))
+
+
+class Todo(db.Model):
+
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    complete = db.Column(db.Boolean)
+
+
+
+    
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
+
+    
+# I will improve this application as I learn more CSS and JS
+ 
+    
+    
+    
+    
+
+
